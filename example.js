54d2289aadb94c6abb6790a601660056168089f1
Brendan Telzrow <telzrowb@gmail.com>
2016-03-24 16:14:40 -0500

    rewrite for zero dependencies

--- example.js
+++ example.js
@@ -1,7 +1,31 @@
-var moneroWallet = require('./lib/wallet.js');
+var moneroWallet = require('./lib/wallet');
 var Wallet = new moneroWallet();
+
+// examples
 
 Wallet.integratedAddress().then(function(result){
     console.log(result);
 });
+
+Wallet.balance().then(function(response){
+    console.log(response);
+});
+
+Wallet.address().then(function(response){
+    console.log(response);
+});
+
+Wallet.height().then(function(height){
+    console.log(height);
+});
+
+//var destination = {};
+//destination.address = '47Vmj6BXSRPax69cVdqVP5APVLkcxxjjXdcP9fJWZdNc5mEpn3fXQY1CFmJDvyUXzj2Fy9XafvUgMbW91ZoqwqmQ6RjbVtp';
+//destination.amount = 1;
+//
+//Wallet.transfer(destination).then(function(result){
+//    console.log(result);
+//});
+
+
 
