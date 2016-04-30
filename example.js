5b67eca55779854e300a126edacc048f694116df
Brendan Telzrow <telzrowb@gmail.com>
2016-04-30 09:35:29 -0500

    version

--- example.js
+++ example.js
@@ -19,6 +19,10 @@ Wallet.height().then(function(height){
     console.log(height);
 });
 
+Wallet.incomingTransfers('all').then(function(result){
+    console.log(result);
+});
+
 //var destination = {};
 //destination.address = '47Vmj6BXSRPax69cVdqVP5APVLkcxxjjXdcP9fJWZdNc5mEpn3fXQY1CFmJDvyUXzj2Fy9XafvUgMbW91ZoqwqmQ6RjbVtp';
 //destination.amount = 1;
