ba040f82b37fd998af288916070db2b94e4e3a67
Brendan Telzrow <telzrowb@gmail.com>
2016-05-29 14:19:26 -0500

    add daemon rpc module

--- example.js
+++ example.js
@@ -1,5 +1,38 @@
 var moneroWallet = require('./lib/wallet');
-var Wallet = new moneroWallet();
+var Daemon = require('./lib/wallet').daemon;
+var daemon = new Daemon();
+
+// execute a GET to your Monero daemon at /get_transaction_pool
+daemon.getTransactionPool().then(function(result) {
+    if(result.status == 'OK' && result.transactions){
+        let transactions = result.transactions;
+
+        // iterate through the transactions and check to see if it contains a payment ID
+        transactions.forEach(function(transaction) {
+            let tx = JSON.parse(transaction.tx_json);
+            
+            // if the first 3 bytes match this, then the following 32 bytes are the payment ID 
+            if(tx.extra[0] == 2 && tx.extra[1] == 33 && tx.extra[2] == 0){
+                let paymentID = '';
+                for(let i = 3; i < 35; i++) {
+                   paymentID += tx.extra[i].toString(16)
+                }
+                // handle the payment ID
+                console.log(`Transaction ${transaction.id_hash} detected with payment ID: ${paymentID}`);
+            } else {
+                // report the tx without payment ID
+                console.log(`Transaction ${transaction.id_hash} does not contain a payment ID.`);
+            } 
+        })
+    } else {
+        console.log(result)
+    }
+})
+
+
+
+
+
 
 // examples
 
@@ -19,9 +52,9 @@ var Wallet = new moneroWallet();
 //     console.log(height);
 // });
 //
-Wallet.incomingTransfers('all').then(function(result){
-    console.log(result);
-});
+// Wallet.incomingTransfers('all').then(function(result){
+//     console.log(result);
+// });
 
 //var destination = {};
 //destination.address = '47Vmj6BXSRPax69cVdqVP5APVLkcxxjjXdcP9fJWZdNc5mEpn3fXQY1CFmJDvyUXzj2Fy9XafvUgMbW91ZoqwqmQ6RjbVtp';
