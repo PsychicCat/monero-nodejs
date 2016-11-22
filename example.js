dde4bacf70610c89ce97e136137ca5119fcdecb8
Brendan Telzrow <btelzrow@techtrav.com>
2016-11-22 16:39:57 -0600

    refactor old methods

--- example.js
+++ example.js
@@ -1,10 +1,14 @@
-// var moneroWallet = require('./lib/wallet');
-// var Wallet = new moneroWallet();
+var moneroWallet = require('./lib/wallet');
+var Wallet = new moneroWallet();
 
 // examples
 
 var utils = require('./lib/utilities');
 
+
+var destinations = [{address: 'ababs', amount: 12.35}, {address: 'jasdfkafls', amount: 12.59}];
+
+console.log(Wallet.transfer(destinations));
 
 
 
