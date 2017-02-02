2316bb2ce693eb70f8359f54f08a5d6bbd440fb6
Brendan Telzrow <telzrowb@gmail.com>
2017-02-01 20:28:04 -0600

    temporary update

--- example.js
+++ example.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
 var moneroWallet = require('./lib/wallet');
-var Wallet = new moneroWallet(process.env.MONERO_HOST);
+var Wallet = new moneroWallet();
 var Util = require('./lib/utilities');
+
 
 // examples
 
-Wallet.balance()
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error));
+// Wallet.balance()
+//     .then((result) => console.log(result))
+//     .catch((error) => console.log(error));
 
 
-
+Wallet.getTransfers({pool: true})
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
 
